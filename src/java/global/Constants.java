2b33ee109a24ab9778c4823a87235bc76705cf78
Teo <cappun@me.com>
2018-09-23 17:25:25 +0200

    Added costants, some idea related settings changed.

---
+++ src/java/global/Constants.java
@@ -0,0 +1,24 @@
+ 
+package global;
+
+public class Constants {
+  
+  /* Constants for Debug */
+  public static final boolean DEBUG=true; 
+  
+  /** Constants for db connection */
+  public static final String DB_USER_NAME = "root";
+  public static final String DB_PASSWORD  = "";
+  public static final String DB_CONNECTION_STRING = "jdbc:mariadb://localhost:3306/Kozel100?user="+DB_USER_NAME+"&password="+DB_PASSWORD;
+
+  /* Constants for log files*/
+  //public final static String LOG_DIR = "/home/pi/log";
+  public static final String LOG_DIR = "/tmp/Kozel100";
+  public static final String FRONTEND_ERROR_LOG_FILE = LOG_DIR + "frontenderror.log";
+  public static final String FATAL_LOG_FILE = LOG_DIR + "fatalerror.log";
+  public static final String GENERAL_LOG_FILE = LOG_DIR + "generalerror.log";  
+  public static final String GENERAL_EXCEPTION_LOG_FILE = LOG_DIR + "generalexception.log";
+  public static final String WARNING_LOG_FILE = LOG_DIR + "warning.log";  
+  public static final String DATABASE_SERVICE_LOG_FILE = LOG_DIR + "databaseservice.log";  
+  
+}
