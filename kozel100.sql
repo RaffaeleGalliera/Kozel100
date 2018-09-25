8da28e93518825fcc33b80b457f42ce8637081ac
Raffaele Galliera <rafforx@gmail.com>
2018-09-25 12:24:49 +0200

    Added libs and .sql files

--- kozel100.sql
+++ kozel100.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 24, 2018 at 05:39 PM
+-- Generation Time: Sep 25, 2018 at 12:22 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -73,6 +73,49 @@ INSERT INTO `contact_person` (`contact_person_id`, `first_name`, `last_name`, `e
 (3, 'dfgdfgd', 'vbvbv', 'bcdfggdgf', '3333', 1, '2018-09-24 15:35:32', '2018-09-24 15:35:32'),
 (4, 'dfsf', 'dfdf', 'sdsadad', '3333', 1, '2018-09-24 15:35:38', '2018-09-24 15:35:38');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `position`
+--
+
+CREATE TABLE `position` (
+  `position_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(42) NOT NULL,
+  `first_name` varchar(15) DEFAULT NULL,
+  `last_name` varchar(15) DEFAULT NULL,
+  `recruitment_date` date DEFAULT NULL,
+  `end_working` date DEFAULT NULL,
+  `phone_number` varchar(10) DEFAULT NULL,
+  `position_id` int(11) DEFAULT NULL,
+  `work_field_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_field`
+--
+
+CREATE TABLE `work_field` (
+  `workd_field_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -92,6 +135,24 @@ ALTER TABLE `contact_person`
   ADD KEY `company_id` (`company_id`);
 
 --
+-- Indexes for table `position`
+--
+ALTER TABLE `position`
+  ADD PRIMARY KEY (`position_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `work_field`
+--
+ALTER TABLE `work_field`
+  ADD PRIMARY KEY (`workd_field_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -106,6 +167,24 @@ ALTER TABLE `company`
 --
 ALTER TABLE `contact_person`
   MODIFY `contact_person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `position`
+--
+ALTER TABLE `position`
+  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `work_field`
+--
+ALTER TABLE `work_field`
+  MODIFY `workd_field_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
