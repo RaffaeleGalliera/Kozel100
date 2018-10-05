ac5a59c8ab9d5f46b640be67125c6cb1939df468
Raffaele Galliera <rafforx@gmail.com>
2018-10-05 15:17:34 +0200

    DB Updated

--- kozel100.sql
+++ kozel100.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 25, 2018 at 12:22 PM
+-- Generation Time: Oct 05, 2018 at 03:16 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `kozel100`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_type`
+--
+
+CREATE TABLE `client_type` (
+  `client_type_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client_type`
+--
+
+INSERT INTO `client_type` (`client_type_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'sssss', '2018-09-27 16:06:14', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -44,9 +64,9 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`company_id`, `name`, `vat`, `address`, `city`, `email`, `created_at`, `updated_at`) VALUES
-(1, 'sssss', 'cxczcxxc', 'aaaaa', 'czcxczcxzx', 'czcxccxccz', '2018-09-23 15:27:24', '2018-09-23 15:27:24'),
-(2, NULL, NULL, NULL, NULL, NULL, '2018-09-23 15:31:48', '2018-09-23 15:31:48'),
-(3, 'ZXsxszczd', 'aaaa', 'ccxczczx', 'ggg', 'fsdfsdf', '2018-09-23 15:32:00', '2018-09-23 15:32:00');
+(1, 'Allegra Garcia', '332323', 'asas', 'sassa', 'ruzyvidufa@mailinator.com', '2018-10-04 07:05:19', '2018-10-04 07:05:19'),
+(2, 'Martena Savage', '343434', 'sdfsdf', 'fsdfsdf', 'rityluvo@mailinator.com', '2018-10-04 07:05:48', '2018-10-04 07:05:48'),
+(3, 'Hamilton Goodman', '343434', 'sadaddd', 'asdsada', 'rixujymir@mailinator.net', '2018-10-04 07:06:07', '2018-10-04 07:06:07');
 
 -- --------------------------------------------------------
 
@@ -70,19 +90,29 @@ CREATE TABLE `contact_person` (
 --
 
 INSERT INTO `contact_person` (`contact_person_id`, `first_name`, `last_name`, `email`, `phone_number`, `company_id`, `created_at`, `updated_at`) VALUES
-(3, 'dfgdfgd', 'vbvbv', 'bcdfggdgf', '3333', 1, '2018-09-24 15:35:32', '2018-09-24 15:35:32'),
-(4, 'dfsf', 'dfdf', 'sdsadad', '3333', 1, '2018-09-24 15:35:38', '2018-09-24 15:35:38');
+(15, 'Iona', 'Daugherty', 'hudyte@mailinator.net', '8168486', 1, '2018-10-04 07:05:19', '2018-10-04 07:05:19'),
+(16, 'Hayes', 'Ray', 'fovagiduh@mailinator.net', '1939057', 2, '2018-10-04 07:05:48', '2018-10-04 07:05:48'),
+(17, 'Jack', 'Sweet', 'hepe@mailinator.com', '8953734', 3, '2018-10-04 07:06:07', '2018-10-04 07:06:07');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `position`
+-- Table structure for table `product_category`
 --
 
-CREATE TABLE `position` (
-  `position_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
+CREATE TABLE `product_category` (
+  `product_category_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product_category`
+--
+
+INSERT INTO `product_category` (`product_category_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'GGGGGG', '2018-10-04 09:52:36', '2018-10-04 09:52:36');
 
 -- --------------------------------------------------------
 
@@ -105,6 +135,17 @@ CREATE TABLE `user` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `email`, `password`, `first_name`, `last_name`, `recruitment_date`, `end_working`, `phone_number`, `position_id`, `work_field_id`, `created_at`, `updated_at`) VALUES
+(1, 'cewug@mailinator.net', 'Pa$$w0rd!', 'Ahmed', 'Guzman', NULL, NULL, NULL, 1, 1, '2018-10-03 13:47:32', '2018-10-03 13:47:32'),
+(2, 'silepifoqe@mailinator.com', 'Pa$$w0rd!', 'Abra', 'Rodriguez', NULL, NULL, NULL, 1, 1, '2018-10-03 13:47:42', '2018-10-03 13:47:42'),
+(3, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, '2018-10-03 13:48:15', '2018-10-03 13:48:15'),
+(4, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, '2018-10-03 13:50:39', '2018-10-03 13:50:39'),
+(5, 'micobaga@mailinator.com', 'Pa$$w0rd!', 'Lev', 'Ayala', NULL, NULL, NULL, 1, 1, '2018-10-03 13:55:44', '2018-10-03 13:55:44');
+
 -- --------------------------------------------------------
 
 --
@@ -112,13 +153,62 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `work_field` (
-  `workd_field_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `work_field_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `work_field`
+--
+
+INSERT INTO `work_field` (`work_field_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'dddd', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
+(2, 'gggg', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
+(3, 'ggggg', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
+(4, 'vcvxcvxc', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
+(5, 'vcvxcvxchhhhccccccccc', '2018-10-05 10:55:33', '2018-10-05 10:55:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_position`
+--
+
+CREATE TABLE `work_position` (
+  `position_id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `work_position`
+--
+
+INSERT INTO `work_position` (`position_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'gggggg', '2018-09-27 15:57:20', '2018-09-27 15:57:20'),
+(3, 'asdafg', '2018-09-27 15:57:20', '2018-09-27 15:57:20'),
+(5, NULL, '2018-09-27 15:57:20', '2018-09-27 15:57:20'),
+(6, NULL, '2018-09-27 15:57:20', '2018-09-27 15:57:20'),
+(7, NULL, '2018-09-27 16:04:21', '2018-09-27 16:04:21'),
+(8, NULL, '2018-09-27 16:04:51', '2018-09-27 16:04:51'),
+(9, NULL, '2018-09-27 16:05:55', '2018-09-27 16:05:55'),
+(10, NULL, '2018-09-27 16:06:14', '2018-09-27 16:06:14'),
+(11, NULL, '2018-09-29 10:11:30', '2018-09-29 10:11:30'),
+(12, NULL, '2018-09-29 10:11:38', '2018-09-29 10:11:38'),
+(13, NULL, '2018-09-29 10:11:43', '2018-09-29 10:11:43');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `client_type`
+--
+ALTER TABLE `client_type`
+  ADD PRIMARY KEY (`client_type_id`);
 
 --
 -- Indexes for table `company`
@@ -135,10 +225,10 @@ ALTER TABLE `contact_person`
   ADD KEY `company_id` (`company_id`);
 
 --
--- Indexes for table `position`
+-- Indexes for table `product_category`
 --
-ALTER TABLE `position`
-  ADD PRIMARY KEY (`position_id`);
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`product_category_id`);
 
 --
 -- Indexes for table `user`
@@ -150,41 +240,53 @@ ALTER TABLE `user`
 -- Indexes for table `work_field`
 --
 ALTER TABLE `work_field`
-  ADD PRIMARY KEY (`workd_field_id`);
+  ADD PRIMARY KEY (`work_field_id`);
+
+--
+-- Indexes for table `work_position`
+--
+ALTER TABLE `work_position`
+  ADD PRIMARY KEY (`position_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `company`
+-- AUTO_INCREMENT for table `client_type`
 --
-ALTER TABLE `company`
-  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `client_type`
+  MODIFY `client_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `contact_person`
 --
 ALTER TABLE `contact_person`
-  MODIFY `contact_person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `contact_person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `position`
+-- AUTO_INCREMENT for table `product_category`
 --
-ALTER TABLE `position`
-  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `product_category`
+  MODIFY `product_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `work_field`
 --
 ALTER TABLE `work_field`
-  MODIFY `workd_field_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `work_field_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `work_position`
+--
+ALTER TABLE `work_position`
+  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
@@ -194,7 +296,7 @@ ALTER TABLE `work_field`
 -- Constraints for table `contact_person`
 --
 ALTER TABLE `contact_person`
-  ADD CONSTRAINT `contact_person_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `contact_person_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
