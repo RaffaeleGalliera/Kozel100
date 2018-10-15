bca2d0cfd0e7e7b6a9f01e13417bccefdb376fbc
Teo <cappun@me.com>
2018-10-15 16:38:04 +0200

    Updated the sql file.

--- kozel100.sql
+++ kozel100.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 12, 2018 at 06:06 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Oct 15, 2018 at 02:37 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -43,11 +43,7 @@ INSERT INTO `client_type` (`client_type_id`, `name`, `created_at`, `updated_at`)
 (1, 'sssss', '2018-09-27 16:06:14', '0000-00-00 00:00:00'),
 (2, 'sdfsdfsdf', '2018-10-07 10:09:27', '0000-00-00 00:00:00'),
 (3, 'Nuovo', '2018-10-07 13:53:37', '0000-00-00 00:00:00'),
-(4, 'Super', '2018-10-12 10:59:27', '0000-00-00 00:00:00'),
-(5, 'Super', '2018-10-12 10:59:27', '0000-00-00 00:00:00'),
-(6, 'Supersss', '2018-10-12 11:00:31', '0000-00-00 00:00:00'),
-(7, 'Supersss', '2018-10-12 11:00:31', '0000-00-00 00:00:00'),
-(8, 'supersantos', '2018-10-12 11:01:35', '0000-00-00 00:00:00');
+(4, 'Buffone', '2018-10-15 13:42:48', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -64,30 +60,44 @@ CREATE TABLE `company` (
   `city` varchar(60) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`company_id`, `name`, `client_type_id`, `vat`, `address`, `city`, `email`, `created_at`, `updated_at`) VALUES
-(1, 'Allegra Garcia', 1, '332323', 'asas', 'sassa', 'ruzyvidufa@mailinator.com', '2018-10-04 07:05:19', '2018-10-07 10:42:45'),
-(2, 'Martena Savage', 1, '343434', 'sdfsdf', 'fsdfsdf', 'rityluvo@mailinator.com', '2018-10-04 07:05:48', '2018-10-07 10:42:59'),
-(3, 'Hamilton Goodman', 1, '343434', 'sadaddd', 'asdsada', 'rixujymir@mailinator.net', '2018-10-04 07:06:07', '2018-10-07 10:42:53'),
-(4, 'Sydnee Cannon', 1, '32424', 'ddd', 'ddddd', 'zyzyxukem@mailinator.net', '2018-10-07 10:26:13', '2018-10-07 10:43:06'),
-(5, 'Abra Peters', 1, '1111', 'ddd', 'dddd', 'binelofumo@mailinator.com', '2018-10-07 10:29:00', '2018-10-07 10:43:13'),
-(6, 'Adele Hancock', 1, '2222222', 'dfsddsd', 'sdfsdfsd', 'pelipyryze@mailinator.net', '2018-10-07 10:35:01', '2018-10-07 10:43:18'),
-(7, 'ZXsxszczd', 1, 'cxczcxxc', 'aaaaa', 'aaaaaa', 'pelipyryze@mailinator.net', '2018-10-07 10:41:19', '2018-10-07 10:41:19'),
-(8, 'Prova', 3, '2222', 'Nuoivo', 'fdlk', 'raf@gkjg.com', '2018-10-07 13:54:23', '2018-10-07 13:54:23'),
-(9, 'DeMaio', 3, '3333', '33333', 'dfdsfds', 'mykif@mailinator.com', '2018-10-09 10:19:54', '2018-10-09 10:19:54'),
-(10, 'Gloria Valentine', 3, '3333', 'fdfdfdf', 'fdfdfdfdf', 'kojap@mailinator.com', '2018-10-12 10:24:05', '2018-10-12 10:24:05'),
-(11, 'Shad Chan', 2, 'ghghggh', 'ghghghg', 'ghghgh', 'xerusow@mailinator.com', '2018-10-12 10:40:11', '2018-10-12 10:40:11'),
-(12, 'Latifah Hood', 3, '111111', 'jkhkjhkj', 'jkhkjhkjh', 'bubemo@mailinator.com', '2018-10-12 10:41:55', '2018-10-12 10:41:55'),
-(13, 'Echo Harrington', 3, '22222', 'sdfsfsdf', 'sdfsdfdf', 'wiwa@mailinator.net', '2018-10-12 10:42:25', '2018-10-12 10:42:25'),
-(14, 'Madeline Hebert', 2, '22222', 'fgdfgdfg', 'dfgdfgdf', 'nuqebeqoj@mailinator.com', '2018-10-12 10:54:53', '2018-10-12 10:54:53'),
-(15, 'Kylynn Oliver', 3, '11111', 'dffsdf', 'sdfgsdfs', 'vahedot@mailinator.net', '2018-10-12 10:55:21', '2018-10-12 10:55:21'),
-(16, 'Finn Santos', 2, 'dffsdf', 'sdfsdfsdf', 'sdfsdf', 'tetybex@mailinator.net', '2018-10-12 10:55:39', '2018-10-12 10:55:39');
+INSERT INTO `company` (`company_id`, `name`, `client_type_id`, `vat`, `address`, `city`, `email`, `created_at`, `updated_at`, `user_id`) VALUES
+(1, 'Allegra Garcia', 1, '332323', 'asas', 'sassa', 'ruzyvidufa@mailinator.com', '2018-10-04 07:05:19', '2018-10-15 10:08:43', 11),
+(2, 'Martena Savage', 1, '343434', 'sdfsdf', 'fsdfsdf', 'rityluvo@mailinator.com', '2018-10-04 07:05:48', '2018-10-07 10:42:59', NULL),
+(3, 'Hamilton Goodman', 1, '343434', 'sadaddd', 'asdsada', 'rixujymir@mailinator.net', '2018-10-04 07:06:07', '2018-10-07 10:42:53', NULL),
+(4, 'Sydnee Cannon', 1, '32424', 'ddd', 'ddddd', 'zyzyxukem@mailinator.net', '2018-10-07 10:26:13', '2018-10-07 10:43:06', NULL),
+(5, 'Abra Peters', 1, '1111', 'ddd', 'dddd', 'binelofumo@mailinator.com', '2018-10-07 10:29:00', '2018-10-07 10:43:13', NULL),
+(6, 'Adele Hancock', 1, '2222222', 'dfsddsd', 'sdfsdfsd', 'pelipyryze@mailinator.net', '2018-10-07 10:35:01', '2018-10-07 10:43:18', NULL),
+(7, 'ZXsxszczd', 1, 'cxczcxxc', 'aaaaa', 'aaaaaa', 'pelipyryze@mailinator.net', '2018-10-07 10:41:19', '2018-10-07 10:41:19', NULL),
+(8, 'Prova', 3, '2222', 'Nuoivo', 'fdlk', 'raf@gkjg.com', '2018-10-07 13:54:23', '2018-10-07 13:54:23', NULL),
+(9, 'DeMaio', 4, '3333', '33333', 'dfdsfds', 'mykif@mailinator.com', '2018-10-09 10:19:54', '2018-10-15 13:43:32', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_product`
+--
+
+CREATE TABLE `company_product` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  `product_category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `company_product`
+--
+
+INSERT INTO `company_product` (`id`, `company_id`, `product_category_id`) VALUES
+(1, 9, 2),
+(2, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -119,19 +129,7 @@ INSERT INTO `contact_person` (`contact_person_id`, `first_name`, `last_name`, `e
 (20, 'Samuel', 'Norton', 'mysas@mailinator.com', '222222', 6, '2018-10-07 10:35:01', '2018-10-07 10:35:01'),
 (21, 'cccc', 'fdfd', 'fuck@gmail.com', '3333', 7, '2018-10-07 10:41:19', '2018-10-07 10:41:19'),
 (22, 'ddfsfs', 'sdffsdf', 'rafffff@raff.it', '3333333', 8, '2018-10-07 13:54:23', '2018-10-07 13:54:23'),
-(23, 'Dominic', 'Fowler', 'tose@mailinator.net', '4444444', 9, '2018-10-09 10:19:54', '2018-10-09 10:19:54'),
-(24, 'Zenaida', 'Mueller', 'tebozewuz@mailinator.com', '886905', 1, '2018-10-12 09:22:40', '2018-10-12 09:22:40'),
-(25, 'Lee', 'Duke', 'gabizo@mailinator.com', '9826468', 5, '2018-10-12 09:26:30', '2018-10-12 09:26:30'),
-(26, 'Martina', 'Good', 'gutaxolezi@mailinator.net', '6644466', 10, '2018-10-12 10:24:05', '2018-10-12 10:24:05'),
-(27, 'Emily', 'Potter', 'rajuqiky@mailinator.com', '6925335', 11, '2018-10-12 10:40:11', '2018-10-12 10:40:11'),
-(28, 'Victoria', 'Cruz', 'xunipes@mailinator.com', '3545419', 12, '2018-10-12 10:41:55', '2018-10-12 10:41:55'),
-(29, 'Dominic', 'Santana', 'kedysabowy@mailinator.com', '9319151', 13, '2018-10-12 10:42:25', '2018-10-12 10:42:25'),
-(30, 'Sybil', 'Willis', 'vimija@mailinator.com', '5673861', 14, '2018-10-12 10:54:53', '2018-10-12 10:54:53'),
-(31, 'Henry', 'Clemons', 'gelubyl@mailinator.net', '5052997', 15, '2018-10-12 10:55:21', '2018-10-12 10:55:21'),
-(32, 'Candice', 'Livingston', 'maqoluca@mailinator.com', '8820388', 16, '2018-10-12 10:55:39', '2018-10-12 10:55:39'),
-(33, 'Merrill', 'Pruitt', 'qysadu@mailinator.net', '645501', 13, '2018-10-12 11:04:46', '2018-10-12 11:04:46'),
-(34, 'Cedric', 'Sutton', 'nukyqi@mailinator.net', '667657', 16, '2018-10-12 11:13:23', '2018-10-12 11:13:23'),
-(35, 'Alfonso', 'Soto', 'dukyton@mailinator.net', '77313', 2, '2018-10-12 11:13:57', '2018-10-12 11:13:57');
+(23, 'Dominic', 'Fowler', 'tose@mailinator.net', '4444444', 9, '2018-10-09 10:19:54', '2018-10-09 10:19:54');
 
 -- --------------------------------------------------------
 
@@ -151,7 +149,8 @@ CREATE TABLE `product_category` (
 --
 
 INSERT INTO `product_category` (`product_category_id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'GGGGGG', '2018-10-04 09:52:36', '2018-10-04 09:52:36');
+(1, 'GGGGGG', '2018-10-04 09:52:36', '2018-10-04 09:52:36'),
+(2, 'Cazzi di Gomma', '2018-10-15 10:59:42', '2018-10-15 10:59:42');
 
 -- --------------------------------------------------------
 
@@ -162,7 +161,6 @@ INSERT INTO `product_category` (`product_category_id`, `name`, `created_at`, `up
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL,
   `password` varchar(42) NOT NULL,
   `first_name` varchar(15) DEFAULT NULL,
   `last_name` varchar(15) DEFAULT NULL,
@@ -171,6 +169,7 @@ CREATE TABLE `user` (
   `phone_number` varchar(10) DEFAULT NULL,
   `position_id` int(11) DEFAULT NULL,
   `work_field_id` int(11) DEFAULT NULL,
+  `is_admin` tinyint(1) DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -179,17 +178,17 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `email`, `is_admin`, `password`, `first_name`, `last_name`, `recruitment_date`, `end_working`, `phone_number`, `position_id`, `work_field_id`, `created_at`, `updated_at`) VALUES
-(1, 'cewug@mailinator.net', 0, 'Pa$$w0rd!', 'Ahmed', 'Guzman', NULL, NULL, NULL, 1, 1, '2018-10-03 13:47:32', '2018-10-03 13:47:32'),
-(2, 'silepifoqe@mailinator.com', 0, 'Pa$$w0rd!', 'Abra', 'Rodriguez', NULL, NULL, NULL, 1, 1, '2018-10-03 13:47:42', '2018-10-03 13:47:42'),
-(3, 'tija@mailinator.com', 0, 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, '2018-10-03 13:48:15', '2018-10-03 13:48:15'),
-(4, 'tija@mailinator.com', 0, 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, '2018-10-03 13:50:39', '2018-10-03 13:50:39'),
-(5, 'micobaga@mailinator.com', 0, 'Pa$$w0rd!', 'Lev', 'Ayala', NULL, NULL, NULL, 1, 1, '2018-10-03 13:55:44', '2018-10-03 13:55:44'),
-(6, 'rafforx@gmail.com', 0, 'mW/OE3Yu+KcHvJoVCh0Gpw==', 'Raffae;e', 'Galliera', NULL, NULL, NULL, 1, 1, '2018-10-07 20:17:49', '2018-10-07 20:17:49'),
-(10, 'wumazajy@mailinator.net', 0, '6RjA0YeAQRaiZANru36n0Q==', 'Veda', 'Waters', NULL, NULL, NULL, 17, 5, '2018-10-10 11:56:00', '2018-10-10 11:56:00'),
-(11, 'qypi@mailinator.net', 0, 'FjNwYM+7oIG6IJN6Cfzebw==', 'Abigail', 'Padilla', NULL, NULL, NULL, 19, 3, '2018-10-12 11:33:16', '2018-10-12 11:33:16'),
-(12, 'admin@adminovic.it', 0, 'PGytuvRI/Jmicfl8uOgxqQ==', 'Aurora', 'Calderon', NULL, NULL, NULL, 22, 4, '2018-10-12 15:50:46', '2018-10-12 15:50:46'),
-(13, 'admin@adminovic.com', 1, 'PGytuvRI/Jmicfl8uOgxqQ==', 'Admin', 'Adminovic', NULL, NULL, NULL, 17, 3, '2018-10-12 16:04:48', '2018-10-12 16:04:48');
+INSERT INTO `user` (`user_id`, `email`, `password`, `first_name`, `last_name`, `recruitment_date`, `end_working`, `phone_number`, `position_id`, `work_field_id`, `is_admin`, `created_at`, `updated_at`) VALUES
+(1, 'cewug@mailinator.net', 'Pa$$w0rd!', 'Ahmed', 'Guzman', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:47:32', '2018-10-03 13:47:32'),
+(2, 'silepifoqe@mailinator.com', 'Pa$$w0rd!', 'Abra', 'Rodriguez', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:47:42', '2018-10-03 13:47:42'),
+(3, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, 0, '2018-10-03 13:48:15', '2018-10-03 13:48:15'),
+(4, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, 0, '2018-10-03 13:50:39', '2018-10-03 13:50:39'),
+(5, 'micobaga@mailinator.com', 'Pa$$w0rd!', 'Lev', 'Ayala', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:55:44', '2018-10-03 13:55:44'),
+(6, 'rafforx@gmail.com', 'mW/OE3Yu+KcHvJoVCh0Gpw==', 'Raffae;e', 'Galliera', NULL, NULL, NULL, 1, 1, 0, '2018-10-07 20:17:49', '2018-10-07 20:17:49'),
+(10, 'wumazajy@mailinator.net', '6RjA0YeAQRaiZANru36n0Q==', 'Veda', 'Waters', NULL, NULL, NULL, 17, 5, 0, '2018-10-10 11:56:00', '2018-10-10 11:56:00'),
+(11, 'cappun@me.com', 'egXzw2hDiTqHGeIXHQkcpQ==', 'Matteo', 'Cappon', NULL, NULL, NULL, 1, 1, 0, '2018-10-11 15:58:47', '2018-10-11 15:58:47'),
+(12, 'muigi@gov.5s', 'IyDBZt1kNEmzKp51YWKYSQ==', 'Luigi', 'Di Maio', NULL, NULL, NULL, 22, 6, 0, '2018-10-12 07:13:04', '2018-10-12 07:13:04'),
+(13, 'cappun@me.com', '2IUr3i1lOo8KDJZ+ytmUlA==', 'awef', 'awef', NULL, NULL, NULL, 1, 1, 0, '2018-10-15 10:29:48', '2018-10-15 10:29:48');
 
 -- --------------------------------------------------------
 
@@ -213,7 +212,8 @@ INSERT INTO `work_field` (`work_field_id`, `name`, `created_at`, `updated_at`) V
 (2, 'gggg', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
 (3, 'ggggg', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
 (4, 'vcvxcvxc', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
-(5, 'vcvxcvxchhhhccccccccc', '2018-10-05 10:55:33', '2018-10-05 10:55:33');
+(5, 'vcvxcvxchhhhccccccccc', '2018-10-05 10:55:33', '2018-10-05 10:55:33'),
+(6, 'Satana', '2018-10-12 07:12:18', '2018-10-12 07:12:18');
 
 -- --------------------------------------------------------
 
@@ -237,11 +237,11 @@ INSERT INTO `work_position` (`position_id`, `name`, `created_at`, `updated_at`) 
 (3, 'asdafg', '2018-09-27 15:57:20', '2018-09-27 15:57:20'),
 (5, 'sdasd', '2018-10-10 11:45:21', '2018-10-10 11:45:21'),
 (17, 'aaaaffff', '2018-10-09 10:41:58', '2018-10-09 10:41:58'),
-(18, NULL, '2018-10-12 11:16:34', '2018-10-12 11:16:34'),
-(19, 'rtyhhfghfgh', '2018-10-12 11:16:38', '2018-10-12 11:16:38'),
-(20, 'rtyhhfghfgh', '2018-10-12 11:16:38', '2018-10-12 11:16:38'),
-(21, 'gfhfghgfh', '2018-10-12 11:16:55', '2018-10-12 11:16:55'),
-(22, 'gfhfghgfh', '2018-10-12 11:16:55', '2018-10-12 11:16:55');
+(21, NULL, '2018-10-12 07:11:51', '2018-10-12 07:11:51'),
+(22, 'Ministro dello sviluppo economico', '2018-10-12 07:12:08', '2018-10-12 07:12:08'),
+(23, NULL, '2018-10-12 10:28:42', '2018-10-12 10:28:42'),
+(24, NULL, '2018-10-15 10:29:06', '2018-10-15 10:29:06'),
+(25, 'Nuovo', '2018-10-15 10:29:17', '2018-10-15 10:29:17');
 
 --
 -- Indexes for dumped tables
@@ -259,7 +259,16 @@ ALTER TABLE `client_type`
 ALTER TABLE `company`
   ADD PRIMARY KEY (`company_id`),
   ADD UNIQUE KEY `name` (`name`),
-  ADD KEY `client_type_id` (`client_type_id`);
+  ADD KEY `client_type_id` (`client_type_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `company_product`
+--
+ALTER TABLE `company_product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `company_id` (`company_id`),
+  ADD KEY `product_category_id` (`product_category_id`);
 
 --
 -- Indexes for table `contact_person`
@@ -302,19 +311,25 @@ ALTER TABLE `work_position`
 -- AUTO_INCREMENT for table `client_type`
 --
 ALTER TABLE `client_type`
-  MODIFY `client_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `client_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `company_product`
+--
+ALTER TABLE `company_product`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `contact_person`
 --
 ALTER TABLE `contact_person`
-  MODIFY `contact_person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `contact_person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `product_category`
 --
 ALTER TABLE `product_category`
-  MODIFY `product_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `product_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -326,13 +341,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `work_field`
 --
 ALTER TABLE `work_field`
-  MODIFY `work_field_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `work_field_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `work_position`
 --
 ALTER TABLE `work_position`
-  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Constraints for dumped tables
@@ -342,7 +357,15 @@ ALTER TABLE `work_position`
 -- Constraints for table `company`
 --
 ALTER TABLE `company`
-  ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`client_type_id`) REFERENCES `client_type` (`client_type_id`);
+  ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`client_type_id`) REFERENCES `client_type` (`client_type_id`),
+  ADD CONSTRAINT `company_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `company_product`
+--
+ALTER TABLE `company_product`
+  ADD CONSTRAINT `company_product_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `company_product_ibfk_2` FOREIGN KEY (`product_category_id`) REFERENCES `product_category` (`product_category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `contact_person`
