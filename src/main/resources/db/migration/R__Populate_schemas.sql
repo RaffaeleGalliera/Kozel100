c2dc41e30696cddccc8f2231fc66f2cb9acb612b
Teo <cappun@me.com>
2018-10-24 16:22:54 +0200

    modified repeatable migration

--- src/main/resources/db/migration/R__Populate_schemas.sql
+++ src/main/resources/db/migration/R__Populate_schemas.sql
@@ -4,17 +4,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-REPLACE INTO `user` (`user_id`, `email`, `password`, `first_name`, `last_name`, `recruitment_date`, `end_working`, `phone_number`, `position_id`, `work_field_id`, `is_admin`, `created_at`, `updated_at`) VALUES
-(1, 'cewug@mailinator.net', 'Pa$$w0rd!', 'Ahmed', 'Guzman', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:47:32', '2018-10-03 13:47:32'),
-(2, 'silepifoqe@mailinator.com', 'Pa$$w0rd!', 'Abra', 'Rodriguez', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:47:42', '2018-10-03 13:47:42'),
-(3, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, 0, '2018-10-03 13:48:15', '2018-10-03 13:48:15'),
-(4, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, 0, '2018-10-03 13:50:39', '2018-10-03 13:50:39'),
-(5, 'micobaga@mailinator.com', 'Pa$$w0rd!', 'Lev', 'Ayala', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:55:44', '2018-10-03 13:55:44'),
-(6, 'rafforx@gmail.com', 'mW/OE3Yu+KcHvJoVCh0Gpw==', 'Raffae;e', 'Galliera', NULL, NULL, NULL, 1, 1, 0, '2018-10-07 20:17:49', '2018-10-07 20:17:49'),
-(10, 'wumazajy@mailinator.net', '6RjA0YeAQRaiZANru36n0Q==', 'Veda', 'Waters', NULL, NULL, NULL, 17, 5, 0, '2018-10-10 11:56:00', '2018-10-10 11:56:00'),
-(11, 'cappun@me.com', 'egXzw2hDiTqHGeIXHQkcpQ==', 'Matteo', 'Cappon', NULL, NULL, NULL, 1, 1, 0, '2018-10-11 15:58:47', '2018-10-11 15:58:47'),
-(12, 'muigi@gov.5s', 'IyDBZt1kNEmzKp51YWKYSQ==', 'Luigi', 'Di Maio', NULL, NULL, NULL, 22, 6, 0, '2018-10-12 07:13:04', '2018-10-12 07:13:04'),
-(13, 'caballo@me.com', '2IUr3i1lOo8KDJZ+ytmUlA==', 'Jonny', 'Prepuzio', NULL, NULL, NULL, 1, 1, 0, '2018-10-15 10:29:48', '2018-10-15 10:29:48');
+--
+-- Dumping data for table `client_type`
+--
 
 REPLACE INTO `client_type` (`client_type_id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Prospect', '2018-09-27 16:06:14', '0000-00-00 00:00:00'),
@@ -22,23 +14,49 @@ REPLACE INTO `client_type` (`client_type_id`, `name`, `created_at`, `updated_at`
 (3, 'Nuovo', '2018-10-07 13:53:37', '0000-00-00 00:00:00'),
 (4, 'Fidelizzato', '2018-10-15 13:42:48', '0000-00-00 00:00:00');
 
-REPLACE INTO `company` (`company_id`, `name`, `client_type_id`, `vat`, `address`, `city`, `email`, `created_at`, `updated_at`, `active_fl`, `user_id`,`product_category_id`) VALUES
-(1, 'Allegra Garcia', 4, '00488410010', 'Via dei cavalli', 'Horseland', 'ruzyvidufab@mailinator.com', '2018-10-04 07:05:19', '2018-10-19 08:14:03', 1, 11,1),
-(2, 'Martena Savage', 1, '07902440010', 'Via 13 Gennaio', 'Tresigallo', 'rityluvo@mailinator.com', '2018-10-04 07:05:48', '2018-10-19 08:15:14', 0, NULL,2),
-(3, 'Hamilton Goodman', 1, '14869599219', 'Viale Fantoccio', 'Malalbergo', 'rixujymir@mailinator.net', '2018-10-04 07:06:07', '2018-10-07 10:42:53', 1, NULL,2),
-(4, 'Sydnee Cannon', 1, '68084061967', 'Via Raffo Gallo', 'Montalbano', 'zyzyxukem@mailinator.net', '2018-10-07 10:26:13', '2018-10-07 10:43:06', 1, NULL,1),
-(5, 'Abra Petersa', 3, '00488410010', 'Piazzale dei Mille', 'Ferrara', 'binelofumo@mailinator.com', '2018-10-07 10:29:00', '2018-10-18 14:18:04', 1, NULL,NULL),
-(6, 'Adele Hancock', 1, '14869599219', 'Via Ludovico Ariosto', 'Argenta', 'pelipyryze@mailinator.net', '2018-10-07 10:35:01', '2018-10-18 14:17:49', 0, NULL,NULL),
-(7, 'CacioCavalloSRL', 1, '07902440010', 'Vicolo Stretto', 'Portomaggiore', 'pelipyryze@mailinator.net', '2018-10-07 10:41:19', '2018-10-17 09:41:18', 0, NULL,NULL),
-(8, 'Prova', 3, '68084061967', 'Corso della Giovecca', 'Tresigallo', 'raf@gkjg.com', '2018-10-07 13:54:23', '2018-10-17 09:39:23', 0, NULL,NULL),
-(9, 'DiMaio', 4, '07902440010', 'Via Manina', 'Pomigliano', 'mykif@mailinator.com', '2018-10-09 10:19:54', '2018-10-17 09:21:49', 0, 11,NULL),
-(10, 'Leah Marks', 4, '14869599219', 'Via Rabbiosa', 'Claviere', 'sywas@mailinator.com', '2018-10-19 09:49:26', '2018-10-19 09:49:26', 1, NULL,NULL),
-(11, 'Napo Orso Capo', 1, '00488410010', 'Via Canalone', 'Gherardi', 'rrr@ddd.com', '2018-10-19 09:55:42', '2018-10-19 09:55:42', 1, NULL,NULL),
-(12, 'Garrison Baird', 3, '68084061967', 'Via Intasata', 'Ferrara', 'qafesibony@mailinator.net', '2018-10-19 09:57:45', '2018-10-19 09:57:45', 1, NULL,NULL);
+--
+-- Dumping data for table `commercial_proposal`
+--
+
+REPLACE INTO `commercial_proposal` (`commercial_proposal_id`, `name`, `description`, `status`, `company_id`, `created_at`, `updated_at`) VALUES
+(1, 'Proposta di prova hehe', '', 'PENDING', 1, '2018-10-24 13:21:19', '2018-10-24 13:21:19');
+
+--
+-- Dumping data for table `company`
+--
+
+REPLACE INTO `company` (`company_id`, `name`, `client_type_id`, `vat`, `address`, `city`, `email`, `created_at`, `updated_at`, `active_fl`, `user_id`, `product_category_id`) VALUES
+(1, 'Allegra Garcia', 4, '00488410010', 'Via dei cavalli', 'Horseland', 'ruzyvidufab@mailinator.com', '2018-10-04 07:05:19', '2018-10-19 08:14:03', 1, 11, 1),
+(2, 'Martena Savage', 1, '07902440010', 'Via 13 Gennaio', 'Tresigallo', 'rityluvo@mailinator.com', '2018-10-04 07:05:48', '2018-10-19 08:15:14', 0, NULL, 2),
+(3, 'Hamilton Goodman', 1, '14869599219', 'Viale Fantoccio', 'Malalbergo', 'rixujymir@mailinator.net', '2018-10-04 07:06:07', '2018-10-07 10:42:53', 1, NULL, 2),
+(4, 'Sydnee Cannon', 1, '68084061967', 'Via Raffo Gallo', 'Montalbano', 'zyzyxukem@mailinator.net', '2018-10-07 10:26:13', '2018-10-07 10:43:06', 1, NULL, 1),
+(5, 'Abra Petersa', 3, '00488410010', 'Piazzale dei Mille', 'Ferrara', 'binelofumo@mailinator.com', '2018-10-07 10:29:00', '2018-10-18 14:18:04', 1, NULL, 0),
+(6, 'Adele Hancock', 1, '14869599219', 'Via Ludovico Ariosto', 'Argenta', 'pelipyryze@mailinator.net', '2018-10-07 10:35:01', '2018-10-18 14:17:49', 0, NULL, 0),
+(7, 'CacioCavalloSRL', 1, '07902440010', 'Vicolo Stretto', 'Portomaggiore', 'pelipyryze@mailinator.net', '2018-10-07 10:41:19', '2018-10-17 09:41:18', 0, NULL, 0),
+(8, 'Prova', 3, '68084061967', 'Corso della Giovecca', 'Tresigallo', 'raf@gkjg.com', '2018-10-07 13:54:23', '2018-10-17 09:39:23', 0, NULL, 0),
+(9, 'DiMaio', 4, '07902440010', 'Via Manina', 'Pomigliano', 'mykif@mailinator.com', '2018-10-09 10:19:54', '2018-10-23 10:15:26', 1, 11, 0),
+(10, 'Leah Marks', 4, '14869599219', 'Via Rabbiosa', 'Claviere', 'sywas@mailinator.com', '2018-10-19 09:49:26', '2018-10-19 09:49:26', 1, NULL, 0),
+(11, 'Napo Orso Capo', 1, '00488410010', 'Via Canalone', 'Gherardi', 'rrr@ddd.com', '2018-10-19 09:55:42', '2018-10-19 09:55:42', 1, NULL, 0),
+(12, 'Garrison Baird', 3, '68084061967', 'Via Intasata', 'Ferrara', 'qafesibony@mailinator.net', '2018-10-19 09:57:45', '2018-10-19 09:57:45', 1, NULL, 0);
+
+--
+-- Dumping data for table `company_tag`
+--
+
+REPLACE INTO `company_tag` (`company_id`, `tag_id`, `created_at`, `updated_at`) VALUES
+(1, 3, '2018-10-22 16:37:35', '2018-10-22 16:37:35');
+
+--
+-- Dumping data for table `consulting_service`
+--
 
 REPLACE INTO `consulting_service` (`consulting_service_id`, `name`, `active_fl`, `created_at`, `updated_at`) VALUES
 (1, 'Consulenza a domicilio', 1, '2018-10-19 13:53:28', '2018-10-19 13:53:28'),
 (2, 'Training Agile', 1, '2018-10-19 13:57:33', '2018-10-19 13:57:33');
+
+--
+-- Dumping data for table `contact_person`
+--
 
 REPLACE INTO `contact_person` (`contact_person_id`, `first_name`, `last_name`, `email`, `phone_number`, `company_id`, `created_at`, `updated_at`, `active_fl`) VALUES
 (15, 'Iona', 'Daugherty', 'hudyte@mailinator.net', '8168486', 1, '2018-10-04 07:05:19', '2018-10-04 07:05:19', 1),
@@ -56,9 +74,79 @@ REPLACE INTO `contact_person` (`contact_person_id`, `first_name`, `last_name`, `
 (27, 'Nicole', 'Simpson', 'zuhi@mailinator.com', '197375', 12, '2018-10-19 09:57:45', '2018-10-19 09:57:45', 1),
 (28, 'Balto', 'Cappon', 'matssd@gmail.cazzo', '3345678', 13, '2018-10-19 12:50:46', '2018-10-19 12:50:46', 1);
 
+--
+-- Dumping data for table `flyway_schema_history`
+--
+
+REPLACE INTO `flyway_schema_history` (`installed_rank`, `version`, `description`, `type`, `script`, `checksum`, `installed_by`, `installed_on`, `execution_time`, `success`) VALUES
+(1, '1', 'Base version', 'SQL', 'V1__Base_version.sql', -322072636, 'root', '2018-10-22 16:20:53', 5318, 1),
+(2, '2', 'Conversation and tag', 'SQL', 'V2__Conversation_and_tag.sql', -260328813, 'root', '2018-10-22 16:20:54', 576, 1),
+(3, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', -1110662359, 'root', '2018-10-22 16:20:54', 175, 1),
+(4, '3', 'Proposal and purchase', 'SQL', 'V3__Proposal_and_purchase.sql', 801620664, 'root', '2018-10-22 18:53:59', 1118, 1),
+(5, '3.0.1', 'Modify id name', 'SQL', 'V3.0.1__Modify_id_name.sql', -1297055543, 'root', '2018-10-22 19:02:46', 14, 1),
+(6, '3.1', 'Proposal service', 'SQL', 'V3.1__Proposal_service.sql', -642314759, 'root', '2018-10-22 20:02:49', 675, 1),
+(7, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', -613471654, 'root', '2018-10-22 21:29:43', 202, 1),
+(8, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', -559192272, 'root', '2018-10-22 21:30:41', 191, 1),
+(9, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', 1727791296, 'root', '2018-10-22 21:36:47', 199, 1),
+(10, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', -1994987520, 'root', '2018-10-23 06:30:08', 143, 1),
+(11, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', 1791276752, 'root', '2018-10-23 06:39:07', 149, 1),
+(12, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', 1755793762, 'root', '2018-10-23 06:48:11', 146, 1),
+(13, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', 1899955602, 'root', '2018-10-23 06:49:40', 209, 1),
+(14, NULL, 'Populate schemas', 'SQL', 'R__Populate_schemas.sql', -1445945243, 'root', '2018-10-23 07:02:40', 207, 1),
+(15, '3.1.1', 'Little Upgrade', 'SQL', 'V3.1.1__Little_Upgrade.sql', 1316650414, 'root', '2018-10-23 08:09:21', 596, 1);
+
+--
+-- Dumping data for table `product_category`
+--
+
 REPLACE INTO `product_category` (`product_category_id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Serramenti e infissi', '2018-10-04 09:52:36', '2018-10-04 09:52:36'),
 (2, 'Software', '2018-10-15 10:59:42', '2018-10-15 10:59:42');
+
+--
+-- Dumping data for table `proposal_service`
+--
+
+REPLACE INTO `proposal_service` (`commercial_proposal_id`, `consulting_service_id`, `created_at`, `updated_at`) VALUES
+(1, 1, '2018-10-24 13:21:41', '2018-10-24 13:21:41'),
+(1, 2, '2018-10-24 13:21:50', '2018-10-24 13:21:50');
+
+--
+-- Dumping data for table `purchase`
+--
+
+REPLACE INTO `purchase` (`company_id`, `consulting_service_id`, `purchase_date`, `start_date`, `created_at`, `updated_at`) VALUES
+(9, 2, '0000-00-00', '0000-00-00', '2018-10-23 09:52:34', '2018-10-23 09:52:34'),
+(9, 1, '2018-10-03', '2018-10-26', '2018-10-23 09:52:55', '2018-10-23 09:52:55'),
+(1, 2, '2018-10-01', '2018-10-25', '2018-10-23 10:26:16', '2018-10-23 10:26:16');
+
+--
+-- Dumping data for table `tag`
+--
+
+REPLACE INTO `tag` (`tag_id`, `name`, `created_at`, `updated_at`) VALUES
+(3, 'Vendita al Dettaglio', '2018-10-22 16:37:22', '2018-10-23 07:04:21'),
+(4, 'Hacker', '2018-10-23 07:04:48', '2018-10-23 07:04:48');
+
+--
+-- Dumping data for table `user`
+--
+
+REPLACE INTO `user` (`user_id`, `email`, `password`, `first_name`, `last_name`, `recruitment_date`, `end_working`, `phone_number`, `position_id`, `work_field_id`, `is_admin`, `created_at`, `updated_at`) VALUES
+(1, 'cewug@mailinator.net', 'Pa$$w0rd!', 'Ahmed', 'Guzman', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:47:32', '2018-10-03 13:47:32'),
+(2, 'silepifoqe@mailinator.com', 'Pa$$w0rd!', 'Abra', 'Rodriguez', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:47:42', '2018-10-03 13:47:42'),
+(3, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, 0, '2018-10-03 13:48:15', '2018-10-03 13:48:15'),
+(4, 'tija@mailinator.com', 'Pa$$w0rd!', 'Jayme', 'Ryan', NULL, NULL, NULL, 5, 5, 0, '2018-10-03 13:50:39', '2018-10-03 13:50:39'),
+(5, 'micobaga@mailinator.com', 'Pa$$w0rd!', 'Lev', 'Ayala', NULL, NULL, NULL, 1, 1, 0, '2018-10-03 13:55:44', '2018-10-03 13:55:44'),
+(6, 'rafforx@gmail.com', 'mW/OE3Yu+KcHvJoVCh0Gpw==', 'Raffae;e', 'Galliera', NULL, NULL, NULL, 1, 1, 0, '2018-10-07 20:17:49', '2018-10-07 20:17:49'),
+(10, 'wumazajy@mailinator.net', '6RjA0YeAQRaiZANru36n0Q==', 'Veda', 'Waters', NULL, NULL, NULL, 17, 5, 0, '2018-10-10 11:56:00', '2018-10-10 11:56:00'),
+(11, 'cappun@me.com', 'egXzw2hDiTqHGeIXHQkcpQ==', 'Matteo', 'Cappon', NULL, NULL, NULL, 1, 1, 1, '2018-10-11 15:58:47', '2018-10-23 10:16:07'),
+(12, 'muigi@gov.5s', 'IyDBZt1kNEmzKp51YWKYSQ==', 'Luigi', 'Di Maio', NULL, NULL, NULL, 22, 6, 0, '2018-10-12 07:13:04', '2018-10-12 07:13:04'),
+(13, 'caballo@me.com', '2IUr3i1lOo8KDJZ+ytmUlA==', 'Jonny', 'Prepuzio', NULL, NULL, NULL, 1, 1, 0, '2018-10-15 10:29:48', '2018-10-15 10:29:48');
+
+--
+-- Dumping data for table `work_field`
+--
 
 REPLACE INTO `work_field` (`work_field_id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Impiegato', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
@@ -67,6 +155,10 @@ REPLACE INTO `work_field` (`work_field_id`, `name`, `created_at`, `updated_at`) 
 (4, 'Consulente', '2018-09-27 15:58:04', '0000-00-00 00:00:00'),
 (5, 'Leader', '2018-10-05 10:55:33', '2018-10-05 10:55:33'),
 (6, 'Boss', '2018-10-12 07:12:18', '2018-10-12 07:12:18');
+
+--
+-- Dumping data for table `work_position`
+--
 
 REPLACE INTO `work_position` (`position_id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'gggggg', '2018-09-27 15:57:20', '2018-09-27 15:57:20'),
